22a36629e1150ca0fd314e4cfabb56bc8f3cc5e0
Юрий <38157715+kiarro@users.noreply.github.com>
2023-01-20 17:34:53 +0300

    fix

--- src/main/java/com/mpi/alienresearch/model/User.java
+++ src/main/java/com/mpi/alienresearch/model/User.java
@@ -153,6 +153,7 @@ public class User implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singleton(getRole());
     }
@@ -168,21 +169,25 @@ public class User implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
         return true;
     }
